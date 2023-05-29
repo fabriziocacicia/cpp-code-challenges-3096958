687f4f10a67fcb53d498a446b24035dacb319314
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-05-29 10:03:27 +0000

    Add the solution to the challenge 6

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -19,6 +19,47 @@ std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
 
     // Write your code here
+    const int COL = 0;
+    const int ROW = 1;
+
+    const char col = knight[0];
+    const char row = knight[1];
+
+    std::vector<std::string> possible_moves;
+
+    for(int i = 0; i < 8; i++) {
+        possible_moves.push_back(knight);
+    }
+
+    possible_moves[0][COL] -= 2;
+    possible_moves[0][ROW] += 1;
+    possible_moves[1][COL] -= 1;
+    possible_moves[1][ROW] += 2;
+    possible_moves[2][COL] += 1;
+    possible_moves[2][ROW] += 2;
+    possible_moves[3][COL] += 2;
+    possible_moves[3][ROW] += 1;
+    possible_moves[4][COL] += 2;
+    possible_moves[4][ROW] -= 1;
+    possible_moves[5][COL] += 1;
+    possible_moves[5][ROW] -= 2;
+    possible_moves[6][COL] -= 1;
+    possible_moves[6][ROW] -= 2;
+    possible_moves[7][COL] -= 2;
+    possible_moves[7][ROW] -= 1;
+
+    for(int i = 0; i < 8; i++) {
+        if(possible_moves[i][COL] < 'a' || possible_moves[i][COL] > 'h') {
+            continue;
+        }
+
+        if(possible_moves[i][ROW] - '0' < 1 || possible_moves[i][ROW] - '0' > 8) {
+            continue;
+        }
+
+        moves.push_back(possible_moves[i]);
+    }
+
 
     return moves;
 }
