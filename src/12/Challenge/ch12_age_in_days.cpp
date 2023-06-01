3455efe8c9fa094bf0a15481b8c6d12603b153ae
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-06-01 14:37:20 +0000

    Add the solution of the challenge 12

--- src/12/Challenge/ch12_age_in_days.cpp
+++ src/12/Challenge/ch12_age_in_days.cpp
@@ -26,6 +26,53 @@ int main(){
         birth_m = 12;
 
     // Write your code here
+    age = 0;
+
+    if(birth_d < 1) {
+        birth_d = 1;
+    }
+
+    time_t now = time(NULL);
+    struct tm nowinfo = *localtime(&now);
+
+    today_y = nowinfo.tm_year + 1900;
+    today_m = nowinfo.tm_mon + 1;
+    today_d = nowinfo.tm_mday;
+    mktime(&nowinfo);
+    std::cout << asctime(&nowinfo) << "\n\n" << std::flush;
+
+    struct tm birth_tm = *localtime(&now);
+    birth_tm.tm_mday = birth_d;
+    birth_tm.tm_mon = birth_m - 1;
+    birth_tm.tm_year = birth_y - 1900;
+    mktime(&birth_tm);
+
+
+    if(today_y == birth_y) {
+       age = nowinfo.tm_yday - birth_tm.tm_yday;
+    } else {
+        for(int y = birth_y; y <= today_y; y++) {
+            bool is_leap = true;
+            if(y % 4 != 0) {
+                is_leap = false;
+            }
+
+            if(y % 100 == 0 && y % 400 != 0) {
+                is_leap = false;
+            }
+
+            int days_in_year = is_leap ? 366 : 365;
+
+            if(y == birth_y) {
+                age += days_in_year - birth_tm.tm_yday;
+            } else if (y == today_y) {
+                 age += nowinfo.tm_yday;
+            } else {
+                age += days_in_year;
+            }
+        }     
+    }
+
 
     if(age < 43830)
         std::cout << "You are " << age << " days old.";
