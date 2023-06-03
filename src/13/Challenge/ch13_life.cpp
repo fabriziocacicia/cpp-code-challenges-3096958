2ead86d9eba92bc406ffdfed83dfd48fc4f522c8
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-06-03 18:04:26 +0000

    Add the solution to the challenge 13

--- src/13/Challenge/ch13_life.cpp
+++ src/13/Challenge/ch13_life.cpp
@@ -50,6 +50,56 @@ int main(){
         std::cout << "\n";
 
         // Write your code here
+        char next_episode_game[N][M];
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                next_episode_game[row][col] = game[row][col];
+            }
+        }
+        // Foreach cell checks how many living neighbours
+        for(int cell_row = 0; cell_row < N; cell_row++) {
+            int top = cell_row == 0 ? N - 1 : cell_row - 1;
+            int bottom = cell_row == N - 1 ? 0 : cell_row + 1;
+
+            for(int cell_col = 0; cell_col < M; cell_col++) {
+                // New cell. Reset all and count again.
+                int neighbours = 0;
+
+                int left = cell_col == 0 ? M - 1 : cell_col - 1;
+                int right = cell_col == M - 1 ? 0 : cell_col + 1;
+
+                int around_rows[3] = {top, cell_row, bottom};
+                int around_cols[3] = {left, cell_col, right};
+
+                for(auto & around_row : around_rows) {
+                    for(auto & around_col : around_cols) {
+                        if(around_row == cell_row && around_col == cell_col) {
+                            continue;
+                        }
+                        if(game[around_row][around_col] == 'X') {
+                            neighbours++;
+                        }
+                    }
+                }
+
+                if(game[cell_row][cell_col] == '-' && neighbours == 3) {
+                    next_episode_game[cell_row][cell_col] = 'X';
+                } 
+
+                if(game[cell_row][cell_col] == 'X') {
+                    if(neighbours < 2 || neighbours > 3) {
+                        next_episode_game[cell_row][cell_col] = '-';
+                    }
+                }
+            }
+        }
+
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                game[row][col] = next_episode_game[row][col];
+            }
+        }
+
         
         std::cout << "Press Enter for the next generation, or type \"Exit\": " << std::flush;
         std::getline(std::cin,go_on);
